718ac6f7b27b8f5ec60bbf8b39a2cda5a31ec548
Josh Hall <shakeyjaystudios@gmail.com>
2017-08-29 06:10:49 -0500

    Initial Init

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(publicPath));
+
+
+
+server.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
